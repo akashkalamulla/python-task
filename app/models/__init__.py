166c8a329773adcdb6b3461dac59aea8e75f8a5d
Akash kalamulla <akashkalamulla1@gmail.com>
2024-05-31 15:43:12 +0530

    update

---
+++ app/models/__init__.py
@@ -0,0 +1,11 @@
+import os
+import importlib
+
+def discover_models():
+  models_dir = os.path.dirname(__file__)
+  for filename in os.listdir(models_dir):
+    if filename.endswith('.py') and filename !='__init__.py':
+      module_name = f'app.modules.{filename[:-3]}'
+      importlib.import_module(module_name)
+
+discover_models()
---
+++ app/models/user.py
@@ -0,0 +1,16 @@
+from .. import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+ __tablename__ = 'users'
+ id = db.Column(db.Integer, primary_key = True)
+ full_name = db.Column(db.String(255), unique=True, nullable=False)
+ email = db.Column(db.String(255), unique=True, nullable=False)
+ Phone = db.Column(db.Integer, unique=True, nullable=False)
+ password = db.Column(db.String(255),nullable=False)
+
+ def __repr__(self):
+  return f'<User: {self.email}>'
+ 
+ def get_id(self):
+  return self.id
